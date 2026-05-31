98732c6a56dca696e4b9afc2d037cc8a331c2eb8
Ayush7Ranjan <aayushranjan48@gamil.com>
2026-05-31 10:01:52 +0530

    learn variable, how to use it, rules

---
+++ 02_variable/index.js
@@ -0,0 +1,25 @@
+console.log("variable")
+
+let a=67 //a container holds 67 . here a is identifier , = is assingment operator and 67 is literal(a contant value it can be string, num etc.)//
+
+
+console.log(a)
+
+// in Java Script you can change the data type at run time just like a=num then a= string//
+
+a="ayush"
+console.log(a)
+
+// rules for variable name 
+// letters, digit, underscore, $ sign allowed.
+//must begin with a $, _ and Letter
+// it is case sensitive
+//JavaScript reserved word cannot be used as a variable name 
+
+let ayush=7
+// let 9ayush=3 it can't allow
+let $ayush=2
+let Ayush=7
+// let var=3 not allowed because var is reserved word
+
+console.log(ayush,$ayush, Ayush) // if you run this code you wil see "Ayush" not equal to "ayush"
